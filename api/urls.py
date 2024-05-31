1edd83e9668c46560b0d7eca3e5bc94dfc475b2a
Mahmoud Ghazy <ma.ghazy.sa@gmail.com>
2024-06-01 02:52:02 +0300

    API-Rest Framework

--- api/urls.py
+++ api/urls.py
@@ -7,4 +7,22 @@ urlpatterns = [
     path('reservations/', api_reservations),
     path('reservations/<int:pk>', api_reservation),
     
+    path('customers/', api_customers),
+    path('customers/<int:pk>', api_customer),
+    
+    path('spaces/', api_spaces),
+    path('spaces/<int:pk>', api_space),
+    
+    path('branchs/', api_branchs),
+    path('branchs/<int:pk>', api_branch),
+    
+    path('rooms/', api_rooms),
+    path('rooms/<int:pk>', api_room),
+    
+    path('tables/', api_tables),
+    path('tables/<int:pk>', api_table),
+    
+    path('desks/', api_desks),
+    path('desks/<int:pk>', api_desk),
+    
 ]
--- api/views.py
+++ api/views.py
@@ -8,13 +8,11 @@ from space.models import *
 
 
 ## Get All Reservations ##
-@api_view()
+@api_view(['GET'])
 def api_reservations(request):
     reservations = Reservation.objects.all()
     serializer = ReservationSerializer(reservations, many=True)
     return Response(serializer.data)
-
-
 
 ## Get Reservation Details ##
 @api_view(['GET'])
@@ -22,3 +20,101 @@ def api_reservation(request,pk):
     reservation =  get_object_or_404(Reservation,pk=pk)
     serializer = ReservationSerializer(reservation)
     return Response(serializer.data)
+
+#************************************************#
+
+## Get All Customers  ##
+@api_view(['GET'])
+def api_customers(request):
+    customers =  Customer.objects.all()
+    serializer = CustomerSerializer(customers,many=True)
+    return Response(serializer.data)
+
+## Get Customer Details ##
+@api_view(['GET'])
+def api_customer(request,pk):
+    customer =  get_object_or_404(Customer,pk=pk)
+    serializer = CustomerSerializer(customer)
+    return Response(serializer.data)
+
+#************************************************#
+
+## Get All Spaces  ##
+@api_view(['GET'])
+def api_spaces(request):
+    spaces =  Space.objects.all()
+    serializer = SpaceSerializer(spaces,many=True)
+    return Response(serializer.data)
+
+## Get Space Details ##
+@api_view(['GET'])
+def api_space(request,pk):
+    space =  get_object_or_404(Space,pk=pk)
+    serializer = SpaceSerializer(space)
+    return Response(serializer.data)
+
+#************************************************#
+
+## Get All Branchs  ##
+@api_view(['GET'])
+def api_branchs(request):
+    branch =  Branch.objects.all()
+    serializer = BranchSerializer(branch,many=True)
+    return Response(serializer.data)
+
+## Get Branch Details ##
+@api_view(['GET'])
+def api_branch(request,pk):
+    branch =  get_object_or_404(Branch,pk=pk)
+    serializer = BranchSerializer(branch)
+    return Response(serializer.data)
+
+#************************************************#
+
+## Get All Rooms  ##
+@api_view(['GET'])
+def api_rooms(request):
+    room =  Room.objects.all()
+    serializer = RoomSerializer(room,many=True)
+    return Response(serializer.data)
+
+## Get Room Details ##
+@api_view(['GET'])
+def api_room(request,pk):
+    room =  get_object_or_404(Room,pk=pk)
+    serializer = RoomSerializer(room)
+    return Response(serializer.data)
+
+#************************************************#
+
+## Get All Tables  ##
+@api_view(['GET'])
+def api_tables(request):
+    table =  Table.objects.all()
+    serializer = TableSerializer(table,many=True)
+    return Response(serializer.data)
+
+## Get Tables Details ##
+@api_view(['GET'])
+def api_table(request,pk):
+    table =  get_object_or_404(Table,pk=pk)
+    serializer = TableSerializer(table)
+    return Response(serializer.data)
+
+#************************************************#
+
+## Get All Desks  ##
+@api_view(['GET'])
+def api_desks(request):
+    desk =  Desk.objects.all()
+    serializer = DeskSerializer(desk,many=True)
+    return Response(serializer.data)
+
+## Get Desk Details ##
+@api_view(['GET'])
+def api_desk(request,pk):
+    desk =  get_object_or_404(Desk,pk=pk)
+    serializer = DeskSerializer(desk)
+    return Response(serializer.data)
+
+#************************************************#

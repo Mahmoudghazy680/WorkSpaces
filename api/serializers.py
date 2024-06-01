1234f313d7d09d53824354e2b0ce03172c3b0b41
Mahmoud Ghazy <ma.ghazy.sa@gmail.com>
2024-06-01 11:46:10 +0300

    API

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from reservation.models import *
 from space.models import *
 
-
+#*********************Reservation***************************#
 class ReservationSerializer(serializers.ModelSerializer):
     """
     Serializer for the Reservation model.
@@ -17,7 +17,7 @@ class ReservationSerializer(serializers.ModelSerializer):
         # fields = ['pk', 'space', 'branch', 'customer', 'price', 'created_at',  ]
         fields = ['pk','booking_id', 'space', 'branch', 'customer','customer_email', 'customer_phone', 'check_in', 'check_out', 'status', 'room_number', 'table_number', 'desk_number', 'price', 'discount', 'coupon', 'created_at', 'updated_at', ]
 
-
+#*********************Space***************************#
 class SpaceSerializer(serializers.ModelSerializer):
     """
     Serializer for the Sapce model.
@@ -29,17 +29,19 @@ class SpaceSerializer(serializers.ModelSerializer):
         fields = ['name','owner','slogan', 'space_adress',]
 
 
+#*********************Customer***************************#
 class CustomerSerializer(serializers.ModelSerializer):
     """
     Serializer for the Customer model.
-    This serializer serializes Customer data including user , related space , first/last name , email, phone , adress, bith date, gender , picture, applied coupons, and last login for this customer.
+
+    This serializer serializes Customer data including user, related space, first/last name, email, phone, address, birth date, gender, picture, applied coupons, and last login for this customer.
     """
     
     class Meta :
         model = Customer
         fields = ['space_name', 'user', 'first_name', 'last_name', 'email', 'phone_number', 'address', 'date_of_birth', 'gender', 'profile_picture', 'applied_coupons', 'last_login', ]
 
-
+#**********************Branch**************************#
 class BranchSerializer(serializers.ModelSerializer):
     """
     Serializer for the Branch model.
@@ -49,29 +51,31 @@ class BranchSerializer(serializers.ModelSerializer):
         model = Branch
         fields = ['space','name','branch_adress', ]
 
-
+#*********************Room***************************#
 class RoomSerializer(serializers.ModelSerializer):
     class Meta :
         model = Room
         fields = ('space','branch','name', 'room_cost', )
 
-
+#*********************Table***************************#
 class TableSerializer(serializers.ModelSerializer):
     class Meta :
         model = Table
         fields = ('space','branch','room', 'name', 'table_cost')
 
+#**********************Desk**************************#
 class DeskSerializer(serializers.ModelSerializer):
     class Meta :
         model = Desk
         fields = ('space','branch','room', 'name', 'desk_cost')
 
-
+#**********************Coupon**************************#
 class CouponSerializer(serializers.ModelSerializer):
     class Meta :
         model = Coupon
         fields = ('space','code','discount_amount', 'expiration_date', 'users')
 
+#************************************************#
 
 
 
